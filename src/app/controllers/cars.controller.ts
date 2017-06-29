8757238d6eaaeb536e7769e306685a5fdbd84a4f
GeorgiNedkov <georgiqwertyuiop@mail.bg>
2017-06-29 17:13:36 +0300

    make it better

--- src/app/controllers/cars.controller.ts
+++ src/app/controllers/cars.controller.ts
@@ -27,12 +27,14 @@ export class CarsController<T> implements BaseController<Car> {
         return this.data.getAll()
             .then((cars: Car[]) => {
                 let makes = ["Acura", "Alfa Romeo", "Aston Martin", "Audi", "Bentley", "BMW", "Bugatti", "Buick", "Cadillac", "Chevrolet8", "Chrysler", "Citroen", "Dodge", "Ferrari", "Fiat", "Ford", "Geely", "General Motors", "GMC", "Honda", "Hyundai", "Infiniti", "Jaguar", "Jeep", "Kia", "Koenigsegg", "Lamborghini", "Land Rover", "Lexus", "Maserati", "Mazda", "McLaren", "Mercedes- Benz", "Mini2", "Mitsubishi", "Nissan", "Pagani", "Peugeot", "Porsche", "Ram", "Renault", "Rolls Royce", "Saab", "Subaru", "Suzuki", "TATA Motors", "Tesla", "Toyota", "Volkswagen", "Volvo"]
-                let prices: [1000, 2000, 3000, 4000, 5000, 6000];
+                let prices1 = [1000, 2000, 3000, 4000, 5000, 7500, 10000, 15000, 20000, 30000, 40000, 50000];
+                let prices2 = [50000, 40000, 30000, 20000, 15000, 10000, 7500, 5000, 4000, 3000, 2000, 1000];
                 const model = {
                     model: cars,
                     user: req.user,
                     makes: makes,
-                    prices: [1000, 2000, 3000, 4000, 5000, 6000]
+                    prices1: prices1,
+                    prices2: prices2
                 };
 
                 return res.render("layout/wellcome", model);
